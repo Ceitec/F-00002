451e30bf98e12e530e81a1bd984fcdaf80ea4e97
Lukáš Novák <xnovak0b@vutbr.cz>
2016-02-10 13:05:53 +0100

    v.1.0
    Základní řídící jednotka - Základní od Michala

---
+++ F-00002/Debug/makedep.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+inc\adc.c
+
+inc\extBus.c
+
+inc\i2c.c
+
+inc\module_ZM.c
+
+inc\packet_processor.c
+
+inc\timer.c
+
+inc\Tribus.c
+
+inc\uart_tri_0.c
+
+inc\uart_tri_1.c
+
+LIC_ZM.c
+
